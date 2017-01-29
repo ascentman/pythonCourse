f3daefe4a567c0f79f9c32a6ee709ffee4c717b2
Volodymyr Rykhva <ascentman@ukr.net>
2017-01-29 23:27:03 +0100

    small change

--- ex14.py
+++ ex14.py
@@ -2,7 +2,6 @@ from sys import argv
 
 script, user_name = argv
 prompt = '>> '
-
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
